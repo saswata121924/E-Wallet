3036bfe8a4678bd7b77844f96ae2c7a8d8b9828f
Saswata Deb <saswata121924@gmail.com>
2023-10-03 23:46:08 +0530

    Removed Security annotation

--- user-service/src/main/java/com/example/ewallet/UserConfig.java
+++ user-service/src/main/java/com/example/ewallet/UserConfig.java
@@ -2,26 +2,15 @@ package com.example.ewallet;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Properties;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
-@EnableWebSecurity
 public class UserConfig {
     Properties getProducerConfigs(){
         Properties properties = new Properties();
